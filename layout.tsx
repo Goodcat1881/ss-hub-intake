f28ebdc773e38402f122cee2365bbe9ccaf59d3f
Goodcat1881 <dashingdivadamas1@gmail.com>
2026-04-02 14:58:58 +0800

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -17,7 +18,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js"
+          strategy="beforeInteractive"
+        />
+        {children}
+      </body>
     </html>
   )
 }
